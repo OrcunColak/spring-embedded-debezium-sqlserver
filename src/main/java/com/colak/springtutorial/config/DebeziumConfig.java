a8042da00382de3a4b7515a741d47f0c79a1ce8b
Orçun Çolak <orcun.colak@hazelcast.com>
2025-02-24 21:38:03 +0300

    first commit

--- src/main/java/com/colak/springtutorial/config/DebeziumConfig.java
+++ src/main/java/com/colak/springtutorial/config/DebeziumConfig.java
@@ -8,60 +8,97 @@ import org.springframework.context.annotation.Configuration;
 public class DebeziumConfig {
 
     // Injecting values from application.yaml using @Value annotation
-    @Value("${debezium.connector.name}")
-    private String connectorName;
 
-    @Value("${debezium.connector.class}")
-    private String connectorClass;
+    @Value("${spring.datasource.hostname}")
+    private String databaseHostName;
 
-    @Value("${debezium.connector.tasks.max}")
-    private String tasksMax;
-
-    @Value("${debezium.connector.database.server.name}")
-    private String databaseServerName;
-
-    @Value("${debezium.connector.database.hostname}")
-    private String databaseHostname;
-
-    @Value("${debezium.connector.database.port}")
+    @Value("${spring.datasource.port}")
     private String databasePort;
 
-    @Value("${debezium.connector.database.user}")
+    @Value("${spring.datasource.username}")
     private String databaseUser;
 
-    @Value("${debezium.connector.database.password}")
+    @Value("${spring.datasource.password}")
     private String databasePassword;
 
-    @Value("${debezium.connector.database.names}")
+    @Value("${spring.datasource.encrypt}")
+    private String databaseEncrypt;
+
+    @Value("${spring.datasource.trust-server-certificate}")
+    private String databaseTrustServerCertificate;
+
+    @Value("${spring.datasource.url}")
+    private String jdbcUrl;
+
+    @Value("${spring.datasource.username}")
+    private String jdbcUser;
+
+    @Value("${spring.datasource.password}")
+    private String jdbcPassword;
+
+    /********************************************************************************************/
+    @Value("${debezium.database.names}")
     private String databaseNames;
 
-    @Value("${debezium.connector.topic.prefix}")
+    @Value("${debezium.database.table.include-list}")
+    private String databaseTableIncludeList;
+
+    @Value("${debezium.topic-prefix}")
     private String topicPrefix;
 
-    @Value("${debezium.connector.database.history.kafka.bootstrap.servers}")
-    private String kafkaBootstrapServers;
+    @Value("${debezium.offset.storage.jdbc.offset-table-name}")
+    private String offsetTableName;
 
-    @Value("${debezium.connector.database.history.kafka.topic}")
-    private String kafkaHistoryTopic;
+    @Value("${debezium.offset.storage.jdbc.offset-table-ddl}")
+    private String offsetTableDdl;
+
+    @Value("${debezium.offset.storage.jdbc.schema-history-table-name}")
+    private String schemaHistoryTableName;
+
+    @Value("${debezium.offset.storage.jdbc.schema-history-table-ddl}")
+    private String schemaHistoryTableDdl;
+
+    @Value("${debezium.offset.storage.jdbc.schema-history-table-exists}")
+    private String schemaHistoryTableExists;
 
     @Bean
     public io.debezium.config.Configuration debeziumConfiguration() {
         // Create the Debezium engine configuration
         return io.debezium.config.Configuration.create()
-                .with("name", connectorName)
-                .with("connector.class", connectorClass)
-                .with("tasks.max", tasksMax)
-                .with("database.server.name", databaseServerName)
-                .with("database.hostname", databaseHostname)
+                .with("name", "embedded-engine")
+                .with("connector.class", "io.debezium.connector.sqlserver.SqlServerConnector")
+                .with("snapshot.mode", "no_data")
+                .with("tasks.max", 1)
+                .with("database.hostname", databaseHostName)
                 .with("database.port", databasePort)
                 .with("database.user", databaseUser)
                 .with("database.password", databasePassword)
-                .with("database.names", databaseNames)
-                .with("database.encrypt", false)
-                .with("database.trustServerCertificate", true)
+                .with("database.encrypt", databaseEncrypt)
+                .with("database.trustServerCertificate", databaseTrustServerCertificate)
+
+                .with("table.include.list", databaseTableIncludeList)
+
                 .with("topic.prefix", topicPrefix)
-                .with("database.history.kafka.bootstrap.servers", kafkaBootstrapServers)
-                .with("database.history.kafka.topic", kafkaHistoryTopic)
+
+                .with("include.schema.changes", false)
+
+                .with("offset.storage", "io.debezium.storage.jdbc.offset.JdbcOffsetBackingStore")
+                .with("offset.storage.jdbc.url", jdbcUrl)
+                .with("offset.storage.jdbc.user", jdbcUser)
+                .with("offset.storage.jdbc.password", jdbcPassword)
+                .with("offset.storage.jdbc.offset.table.name", offsetTableName)
+                .with("offset.storage.jdbc.offset.table.ddl", offsetTableDdl)
+                .with("offset.flush.interval.ms", 10000)
+
+                .with("schema.history.internal", "io.debezium.storage.jdbc.history.JdbcSchemaHistory")
+                .with("schema.history.internal.jdbc.url", jdbcUrl)
+                .with("schema.history.internal.jdbc.user", jdbcUser)
+                .with("schema.history.internal.jdbc.password", jdbcPassword)
+                .with("schema.history.internal.jdbc.schema.history.table.name", schemaHistoryTableName)
+                .with("schema.history.internal.jdbc.schema.history.table.ddl", schemaHistoryTableDdl)
+                .with("schema.history.internal.jdbc.schema.history.table.exists", schemaHistoryTableExists)
+                .with("schema.history.internal.store.only.captured.tables.ddl", true)
+
                 .build();
 
     }
